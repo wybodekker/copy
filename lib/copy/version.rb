2f983258698176858f29d8e649b5a6b7250519cf
wybo <devnull@localhost>
2012-11-28 22:58:08 +0100

    Initial revision

---
+++ lib/copy/version.rb
@@ -0,0 +1,10 @@
+# Sets constant Copy::VERSION, the version number of
+# this package. It is used in Gem creation
+# For required files: can also be consulted after require'ing
+# For executable files: is used for the --version option.
+module Copy
+  # The current version of this package
+  VERSION = '1.00'
+end
+
+# $Id$
